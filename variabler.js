04245f9894740a0a5f1b998e7e550e6d2361b7b9
David-jrh <5123693@rts-365.dk>
2018-01-19 11:58:31 +0100

    opgave

---
+++ variabler.js
@@ -0,0 +1,49 @@
+// Variable Øvelser
+
+// 2
+// let navn = "kage";
+// console.log(navn);
+
+
+// 3
+// let alder = 25;
+// console.log(alder);
+
+// 4 (n ska være stort)
+// let byNavn = "Roskilde";
+// console.log(byNavn);
+
+// 5
+// let x = 40;
+// let y = 2;
+// console.log(x + y);
+
+// 6
+// let x = 5;
+// let y = 10;
+// let z = x + y;
+// console.log(z);
+
+
+// 7
+// let mitEfternavn = "Hjorth";
+// let minAlder = "25";
+// let mitFornavn = "David";
+// console.log('Hej, jeg hedder ' + mitFornavn + ' ' + mitEfternavn + ' og jeg er ' + minAlder + ' år');
+
+// 8
+// let moms = 1.25;
+// let prisUdenMoms = 400;
+// let prisMedMoms = prisUdenMoms * moms;
+// let prisUdenMoms2 = prisMedMoms / moms;
+// console.log(prisMedMoms, prisUdenMoms2);
+
+// 9
+// let forste = 10;
+// let anden = 20;
+
+// let kage = forste;
+// forste = anden;
+// anden = kage;
+// console.log(forste, anden);
+
