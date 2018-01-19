a4d2d903df3ae6d8123a85e3cc992e58eb2fb84c
David-jrh <5123693@rts-365.dk>
2018-01-19 13:24:36 +0100

    aks

---
+++ betingelser.js
@@ -0,0 +1,47 @@
+// betingelser øvelser
+
+// 3
+// let hej = true;
+
+// if (1) {
+//     console.log('hej verden');
+// };
+
+// 3
+
+// let random = Math.floor(Math.random() * 11);
+
+// if (random > 5) {
+//     console.log("støre end fem");
+// }
+// else {
+//     console.log("mindre end fem");
+// }
+
+
+// 4
+// let yes = true;
+// let result = yes ? "maybe" : "no";
+// console.log(result);
+
+
+// 5
+// let laeskDrik = "kakao";
+
+// if (laeskDrik == "kakao") {
+//     console.log('er det bedste');
+
+// } else if (laeskDrik == "fisk") {
+//     console.log('kagemad');
+
+// } else if (laeskDrik == "ost") {
+//     console.log('halvt');
+
+// } else if (laeskDrik == "løg") {
+//     console.log('godt');
+
+// } else {
+//     console.log('nej');
+// }
+
+6
